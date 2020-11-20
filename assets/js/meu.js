1552df890a0f3118dce3a4d4fca0a4f0f799f6ed
rapha <raphaelalvesoliver998@gmail.com>
2020-11-20 20:49:15 -0300

    Dia 1 - Home
    Layout da página home

---
+++ assets/js/meu.js
@@ -0,0 +1,70 @@
+var el = document.getElementById('menu'); // elemento alvo
+var ele = document.querySelectorAll('.nav-link');
+var btn = document.getElementById('btn-entrar');
+var icon = document.getElementById('icon');
+var logo = document.getElementById('logo');
+var numPx = '50'; // Quantidade de pixels a contar do TOP até definir a cor
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > numPx) {
+    el.classList.remove('navbar-dark');
+    el.classList.add('mudaCor'); // adiciona classe "mudaCor"
+    el.classList.add('navbar-light');
+    el.classList.add('shadow-bottom')
+    btn.classList.remove('text-white');
+    btn.classList.add('text-black');
+
+    icon.src = "assets/img/entrar_black.svg"
+    logo.src = "assets/img/logo_black.svg";
+
+    ele.forEach(element =>{
+        element.classList.replace('text-white','text-black');
+    });
+
+  } else {
+    el.classList.remove('mudaCor'); // remove classe "mudaCor"
+    el.classList.remove('navbar-light');
+    el.classList.remove('shadow-bottom');
+    el.classList.add('navbar-dark');
+    btn.classList.add('text-white');
+
+    icon.src = "assets/img/mdi_person_outline.svg";
+    logo.src = "assets/img/logo_white.svg";
+
+    ele.forEach(element =>{
+      element.classList.replace('text-black','text-white');
+    });
+  }
+});
+
+    /**
+     * Replace all SVG images with inline SVG
+     */
+    jQuery('img.svg').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+
+      jQuery.get(imgURL, function(data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
+
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+              $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
+
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
+
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+
+      }, 'xml');
+
+  });
