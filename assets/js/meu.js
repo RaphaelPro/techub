d01bdbaa5e025449d8148a8921d7698765affd00
rapha <raphaelalvesoliver998@gmail.com>
2020-11-22 03:01:57 -0300

    Desenvolvimento do site

--- assets/js/meu.js
+++ assets/js/meu.js
@@ -36,35 +36,29 @@ window.addEventListener('scroll', function() {
     });
   }
 });
+    
+var big = true;
+var pac = document.getElementById('paco');
+var pc2 = document.getElementById('paco2');
 
-    /**
-     * Replace all SVG images with inline SVG
-     */
-    jQuery('img.svg').each(function(){
-      var $img = jQuery(this);
-      var imgID = $img.attr('id');
-      var imgClass = $img.attr('class');
-      var imgURL = $img.attr('src');
+pac.onclick = function () {
+    if(big){
+      alert('Diminui');
+        big = false;
+        var anime 
+        document.getElementById('saiba').style.display = "none";
+        pac.style.height = "60px";
+        pac.style.width = "400px";
+    }else{
+      document.getElementById('saiba').style.display = "block";
+      alert('cresce');
+      big = true;
+      pac.style.height = "560px";
+      pac.style.width = "400px";
+    }    
+}
 
-      jQuery.get(imgURL, function(data) {
-          // Get the SVG tag, ignore the rest
-          var $svg = jQuery(data).find('svg');
 
-          // Add replaced image's ID to the new SVG
-          if(typeof imgID !== 'undefined') {
-              $svg = $svg.attr('id', imgID);
-          }
-          // Add replaced image's classes to the new SVG
-          if(typeof imgClass !== 'undefined') {
-              $svg = $svg.attr('class', imgClass+' replaced-svg');
-          }
+  
 
-          // Remove any invalid XML tags as per http://validator.w3.org
-          $svg = $svg.removeAttr('xmlns:a');
 
-          // Replace image with new SVG
-          $img.replaceWith($svg);
-
-      }, 'xml');
-
-  });
